83fa15f01d49af757fb3c00e3ca445a84f3ef8d4
dnxv <camacho.dc115@gmail.com>
2024-06-02 23:35:20 -0700

    updated intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,35 @@ import React from "react";
 
 function Intro() {
   return (
-    <div>introduction</div>
+    <div className="flex items-center 
+    justify-center flex-col text-center
+    pt-20 pb-6">
+      
+      <h1 className="font-bold 
+      text-4xl md:text-7xl 
+      mb-1     md:mb-3">
+        Dave.dev
+      </h1>
+
+      {/*  text-base: 1rem, line-height 1.5rem  */}
+      <p className="text-base font-medium
+      md:text-xl
+      mb-3">
+        Software Engineer, Robotics, AI/Machine Learning
+      </p>
+
+      {/*  max-w-xl: max-width-36rem  */}
+      <p className="text-sm max-w-xl font-bold
+      mb-6">
+        Welcome to my portfolio! I'm a Master's student 
+        studying Computer Science.
+        Some of my projects include AI/machine learning, 
+        computer vision, robotics, and web development.
+
+
+      </p>
+
+    </div>
   )
 }
 
