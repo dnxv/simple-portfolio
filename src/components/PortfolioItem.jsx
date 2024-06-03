00d7107ad9702f244974077d2de07bad96857fd7
dnxv <camacho.dc115@gmail.com>
2024-06-03 00:22:17 -0700

    updated portfolio section

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 function PortfolioItem({title, imgUrl, stack, link}) {
   return (
-    <div>
+    <div className="border-2 border-stone-900
+    rounded-md overflow-hidden">
 
       <img src={imgUrl} 
       alt="portfolio"
@@ -11,10 +12,23 @@ function PortfolioItem({title, imgUrl, stack, link}) {
       object-cover cursor-pointer"/>
 
       <div className="w-full p-4">
-        <h3>{title}</h3>
-        <p>
+        <h3 className="
+        text-lg md:text-xl
+        mb-2 md:mb-3
+        font-semibold">
+          {title}
+        </h3>
+        
+        <p className="flex flex-wrap gap-2
+        flex-row items-center justify-start
+        text-xs md:text-sm">
           {stack.map(item =>
-            <span>{item}</span>
+            <span className="inline-block
+            px-2 py-1 border-2
+            border-stone-900 rounded-md
+            font-semibold">
+              {item}
+            </span>
           )}
         </p>
       </div>
