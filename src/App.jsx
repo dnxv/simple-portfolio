29c868722c592965ba4b05a7ed3dd7d6ad03fb63
dnxv <camacho.dc115@gmail.com>
2024-06-05 23:41:33 -0700

    dark mode button

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,11 @@ function App() {
 		<button
 			type="button"
 			onClick={handleThemeSwitch}
-			className="fixed p-2 z-10 right-20 top-4 bg-violet-300 dark:bg-orange-300 text-lg p-1 rounded-md"
+			className="fixed p-1 z-10 
+                top-2 right-2 
+                bg-violet-300 
+                dark:bg-orange-300 
+                text-lg p-1 rounded-md"
 		>
 			{theme === 'dark' ? sun : moon}
 		</button>
