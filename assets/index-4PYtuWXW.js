570145e1f254738385c7fcd4778f6cb0ae253ebc
dnxv <dnxv@users.noreply.github.com>
2024-06-07 17:24:25 +0000

    deploy: f9d1b1d938c9078e4ffdcf3e2304f8adf02be9a7

--- assets/index-4PYtuWXW.js
+++ assets/index-4PYtuWXW.js
@@ -100,13 +100,16 @@ Error generating stack: `+o.message+`
           w-full md:w-7/12`,children:[x.jsx("input",{type:"hidden",name:"_captcha",value:"false"}),x.jsx("input",{type:"hidden",name:"_next",value:"https://dnxv.github.io/simple-portfolio/"}),x.jsx(nc,{children:"Contact"}),x.jsx("input",{type:"text",name:"name",placeholder:"Name",className:`p-2 bg-transparent
             border-2 rounded-md
             focus:outline-none
+            border-stone-500
             dark:border-neutral-600`}),x.jsx("input",{type:"text",name:"email",placeholder:"Email",className:`my-2 p-2 bg-transparent
             border-2 rounded-md
             focus:outline-none
+            border-stone-500
             dark:border-neutral-600`}),x.jsx("textarea",{name:"message",placeholder:"Message",rows:"10",className:`p-2 mb-4
             bg-transparent
             border-2 rounded-md
             focus:outline-none
+            border-stone-500
             dark:border-neutral-600`}),x.jsx("button",{type:"submit",className:`text-center inline-block
             px-8 py-3 w-max 
             text-base font-medium text-white
